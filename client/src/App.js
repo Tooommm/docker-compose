bd2e44e1e966ae697fa3fa1d7183a56bcfd70f1a
Thomas Brouillet <thomasbrouillet@gmail.com>
2022-01-17 23:25:23 +0100

    git ignore

--- client/src/App.js
+++ client/src/App.js
@@ -15,7 +15,7 @@ function App() {
                     target="_blank"
                     rel="noopener noreferrer"
                 >
-                    Learn React and so much more
+                    Learn cool stuff and so much more and more
                 </a>
             </header>
         </div>
